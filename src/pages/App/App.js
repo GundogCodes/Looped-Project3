9560ecb5fc4fc8296f14b07b436389fb70d88dbe
Gunish <gunishsharma20@gmail.com>
2023-08-15 14:57:39 -0600

    added App page in pages and set up indexJS properly with router

---
+++ src/pages/App/App.js
@@ -0,0 +1,12 @@
+import LoginForm from "../../components/loginForm/loginForm"
+import NavBar from "../../components/NavBar/NavBar"
+import routes from '../../router/index';
+
+export default function App(props){
+    return(
+        <div>
+        <NavBar routes={routes} />    
+        <LoginForm />
+        </div>
+    )
+}
