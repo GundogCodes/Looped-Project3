381bda09f226f846dc8f908480ffc1d2183f9ceb
Gunish <gunishsharma20@gmail.com>
2023-08-15 13:43:55 -0600

    ensure loggedIn is not working its not reading reqUser

--- app-server.js
+++ app-server.js
@@ -23,9 +23,10 @@ app.use(logger('dev'))
 app.use(favicon(path.join(__dirname, 'public','img', 'logo.png')))
 app.use(express.static(path.join(__dirname, 'public'))) 
 
-app.use('/api/users', userRoutes )
-app.use('/api/items' , itemRoutes )
-app.use('/api/orders', orderRoutes)
+const ensureLoggedIn = require('./config/ensureLoggedIn')
+app.use('/api/users',ensureLoggedIn ,userRoutes )
+app.use('/api/items' , ensureLoggedIn,itemRoutes )
+app.use('/api/orders',ensureLoggedIn, orderRoutes)
 
 app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
