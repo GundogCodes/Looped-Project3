5b2959768041b98a912baf208e9ac3845c29021c
Gunish <gunishsharma20@gmail.com>
2023-08-16 15:56:25 -0600

    added all components and their css signUpForm error tho

---
+++ src/components/MenuList/MenuList.js
@@ -0,0 +1,16 @@
+import styles from './MenuList.module.scss'
+import MenuListItem from '../MenuListItem/MenuListItem'
+
+export default function MenuList({menuItems, handleAddToOrder}){
+    const items = menuItems.map(item =>
+        <MenuListItem
+        key={item._id}
+        handleAddToOrder={handleAddToOrder}
+        menuItems={item}
+        />)
+        return(
+            <main className={styles.MenuList}>
+                {items}
+            </main>
+        )
+}
--- src/components/SignupForm/SignUpForm.js
+++ src/components/SignupForm/SignUpForm.js
@@ -1,15 +1,56 @@
 import styles from './SignUpForm.module.scss'
-export default function SignUp(){
-    return(
-        <div>
-            <form className={styles.signUpForm}>
+import {Component} from 'react'
+import {signUp} from '../../../utilities/users-service'
 
-                <p className={styles.text}>Username </p><input type='text' className={styles.username} />
-                <p className={styles.text}>email </p><input type='text' className={styles.email} />
-                <p className={styles.text}>Password </p><input type='text ' className={styles.password} />
-
-                <input type='submit' value='Login' className={styles.createAccountButton} />
-            </form>
-        </div>
-    )
+export default class SignUpForm extends Component{
+    state = {
+        name:'',
+        email:'',
+        password:'',
+        confirm:'',
+        error:''
+    }
 }
+
+handleChange = (evt) =>{
+    this.setState({
+        [evt.target.name]:evt.target.value,
+        error:''
+    })
+} 
+
+handleSubmit = async (evt) =>{
+    evt.preventDefault()
+    try {
+        const formData = {...this.state}
+        delete formData.confirm
+        delete formData.error
+        const user = await signUp(formData)
+        this.props.setUser(user)
+    } catch (error) {
+        this.setState({ error: 'Sign Up Failed - Try Again' });
+    }
+}
+
+render() {
+    const disable = this.state.password !== this.state.confirm;
+    return (
+      <div>
+        <div className="form-container">
+          <form autoComplete="off" onSubmit={this.handleSubmit}>
+            <label>Name</label>
+            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+            <label>Email</label>
+            <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+            <label>Password</label>
+            <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+            <label>Confirm</label>
+            <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+            <button type="submit" disabled={disable}>SIGN UP</button>
+          </form>
+        </div>
+        <p className="error-message">&nbsp;{this.state.error}</p>
+      </div>
+    );
+  }
+  }
