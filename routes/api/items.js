2cfff15cf90047630dd347f2c06f2dad0bc1db1b
Gunish <gunishsharma20@gmail.com>
2023-08-15 18:16:18 -0600

    backend routes all seem to work

--- routes/api/items.js
+++ routes/api/items.js
@@ -9,16 +9,3 @@ router.get('/:id', itemsCtrl.show);
 
 module.exports = router;
 
-/*
-router.get('/', itemsCtrl.seeAllItems) //works
-
-router.get('/:id', itemsCtrl.seeAnItem) //works
-
-router.post('/new', itemsCtrl.createItem) //works
-
-router.put('/:id', itemsCtrl.updateItem) //works
-
-router.delete('/:id', itemsCtrl.deleteItem) //works
-
-module.exports =  router
-*/
--- routes/api/users.js
+++ routes/api/users.js
@@ -14,39 +14,3 @@ router.get('/check-token', ensureLoggedIn, checkToken)
 
 module.exports = router
 
-/*
-
-router.get('/', userCtrl.seeAllUsers)
-// POST /api/users
-router.post('/', userCtrl.createUser)
-// POST /api/users/login
-router.post('/login', userCtrl.login)
-router.delete('/:id', userCtrl.auth, userCtrl.deleteUser)
-router.put('/:id', userCtrl.auth, userCtrl.updateUser)
-router.get('/:id', userCtrl.showAUser)
-
-router.get('/', userCtrl.seeAllUsers) //works
-router.get('/:id', userCtrl.auth, userCtrl.showAUser) //works
-router.get('/user/')
-router.post('/new', userCtrl.createUser) //works
-router.post('/login',  userCtrl.loginUser) //works
-router.delete('/:id', userCtrl.auth, userCtrl.deleteUser) //works
-router.put('/:id', userCtrl.auth, userCtrl.updateUser) //works
-
-module.exports = router
-
-seeUserCart
-clearCart
-addItemToCart
-deleteItemInCart
-updateItemInCart
- */
-
-/*
-    "name":"Oppenhimer",
-    "price":60,
-    "category":"nuclearPower",
-    "itemID":"423",
-    "quantity":1
-}
-*/
