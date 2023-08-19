7eb6e5271461992860aef0ba3e06316d2adeb50f
Gunish <gunishsharma20@gmail.com>
2023-08-18 23:40:15 -0600

    adding photos to seed

--- src/components/MenuListItem/MenuListItem.js
+++ src/components/MenuListItem/MenuListItem.js
@@ -3,7 +3,7 @@ import styles from './MenuListItem.module.scss'
 export default function MenuListItem({menuItem, handleAddToOrder}){
     return(
         <div className={styles.MenuListItem}>
-            <div className={styles.imageUrl}>{menuItem.imageUrl}</div>
+            <img src={menuItem.imageUrl} alt="img" className={styles.imageUrl}/>
             <div className={styles.name}>{menuItem.name}</div>
             <div className={styles.buy}></div>
             <span>${menuItem.price.toFixed(2)}</span>
